1e8fbafbb674bc7442a4ae881fa04da2875d046b
ericknovais <erick.hora@Outlook.com>
2022-11-25 19:33:19 -0300

    05.TSC

--- typescript-curso-1-arquivos-iniciais/app/app.ts
+++ typescript-curso-1-arquivos-iniciais/app/app.ts
@@ -2,10 +2,15 @@ import { NegociacaoController } from "./controllers/negociacao-controller.js";
 
 
 const controller = new NegociacaoController();
-const form = document.querySelector('form');
+const form = document.querySelector('.form');
+if (form) {
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        controller.adiciona();
+    });
+}
+else{
+    throw Error('Não foi possível inicializar a aplicação. Verifique se o form existe.')
+}
 
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    controller.adiciona();
-})
 
