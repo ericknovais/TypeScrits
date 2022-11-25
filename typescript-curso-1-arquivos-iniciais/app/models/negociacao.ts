ad3290f5fc34bf8c7386b346b5c8330fa807124c
ericknovais <erick.hora@Outlook.com>
2022-11-25 18:43:33 -0300

    03.VisibilidadeDeMetodos

--- typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
+++ typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
@@ -4,11 +4,11 @@ export class Negociacao {
         public readonly quantidade: number,
         public readonly valor: number) { }
 
-    get volume(): number {
+    public get volume(): number {
         return this.quantidade * this.valor;
     }
 
-    get data(): Date {
+    public get data(): Date {
         const data = new Date(this._data.getTime());
         return data;
     }
--- typescript-curso-1-arquivos-iniciais/app/views/negociacoes-view.ts
+++ typescript-curso-1-arquivos-iniciais/app/views/negociacoes-view.ts
@@ -3,7 +3,7 @@ import { View } from "./view.js";
 
 export class NegociacoesView extends View<Negociacoes> {
 
-    template(model: Negociacoes): string {
+    protected template(model: Negociacoes): string {
         return `
         <table class="table table-hover  table-bordered">
             <thead>
@@ -14,17 +14,21 @@ export class NegociacoesView extends View<Negociacoes> {
                 </tr>
             </thead>
             <tbody>
-                ${model.lista().map(negociacao =>{
-                    return `
+                ${model.lista().map(negociacao => {
+            return `
                         <tr>
-                            <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                            <td>${this.formataData(negociacao.data)}</td>
                             <td>${negociacao.quantidade}</td>
                             <td>${negociacao.valor}</td>
                         </tr>
                     `
-                }).join('')}
+        }).join('')}
             </tbody>
         </table>
         `;
+    }
+
+    private formataData(data: Date): string {
+        return new Intl.DateTimeFormat().format(data);
     }
 }
