78f4d908e68ec2460e26f6c9f6071836c2ddd2f8
ericknovais <erick.hora@Outlook.com>
2022-11-25 21:27:54 -0300

    Parte3_01.EntendendoDecorators

--- typescript-curso-1-arquivos-iniciais/app/src/views/view.ts
+++ typescript-curso-1-arquivos-iniciais/app/src/views/view.ts
@@ -1,3 +1,5 @@
+import { logarRempoDeExecucao } from '../decorators/logar-tempo-de-execucao.js';
+
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
@@ -15,12 +17,10 @@ export abstract class View<T> {
         }
     }
 
-    public update(model: T): void {
+    @logarRempoDeExecucao()
+    public update({ model }: { model: T; }): void {
         let template = this.template(model);
-        if (this.escapar) {
-            template = template
-                .replace(/<script>[\s\S]*?<\/script>/, '');
-        }
+        if (this.escapar) { template = template.replace(/<script>[\s\S]*?<\/script>/, ''); }
         this.elemento.innerHTML = template;
     }
 
