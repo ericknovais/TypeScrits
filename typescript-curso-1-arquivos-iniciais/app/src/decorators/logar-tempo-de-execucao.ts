04ffd49ca1393d9d1912e21a4efe7f22ecb3438c
ericknovais <erick.hora@Outlook.com>
2022-11-27 14:49:58 -0300

    02.DecoratorsDeMetodos

--- typescript-curso-1-arquivos-iniciais/app/src/decorators/logar-tempo-de-execucao.ts
+++ typescript-curso-1-arquivos-iniciais/app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,16 +1,23 @@
-export function logarRempoDeExecucao() {
+export function logarRempoDeExecucao(emSegundos: boolean = false) {
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-        
         const metodoOriginal = descriptor.value;      
         descriptor.value = function (...args: any[]) {
+            
+            let divisor = 1;
+            let unidade ='milisegundos';
+            
+            if(emSegundos){ 
+                divisor = 1000;
+                unidade = 'segundos';
+            }
+
             const t1 = performance.now();
             ///chamar o método original
             const retorno = metodoOriginal.apply(this, args);
             const t2 = performance.now();
-            console.log(`${propertyKey}, Tempo de execução: ${(t2 - t1) / 1000} segundos`);
+            console.log(`${propertyKey}, Tempo de execução: ${(t2 - t1) / divisor} ${unidade}`);
             retorno;
         }
-
         return descriptor;
     }
 }
